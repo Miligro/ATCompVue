f5b7db61f793a549d2ab242f18aa041c7159287b
Miligro <milo20054@gmail.com>
2022-07-12 15:28:12 +0200

    Add HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,40 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
 
 <template>
-  <main></main>
+  <div class="main">
+    <h1>Strona główna</h1>
+    <hr />
+    <div class="main-page-navs">
+      <RouterLink to="/form">
+        <div class="navigation-con main-page-nav" id="posts_btn">
+          <p>Formularz</p>
+          <font-awesome-icon icon="fa-brands fa-wpforms" />
+        </div>
+      </RouterLink>
+      <RouterLink to="/posts">
+        <div class="navigation-con main-page-nav" id="posts_btn">
+          <p>Posty</p>
+          <font-awesome-icon icon="fa-regular fa-message" />
+        </div>
+      </RouterLink>
+      <RouterLink to="/albums">
+        <div class="navigation-con main-page-nav" id="posts_btn">
+          <p>Albumy</p>
+          <font-awesome-icon icon="fa-regular fa-images" />
+        </div>
+      </RouterLink>
+    </div>
+  </div>
 </template>
+
+<style>
+hr {
+  width: 30%;
+}
+
+h1 {
+  font-size: 50px;
+}
+</style>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 button {
   padding: 10px;
   width: 20%;
